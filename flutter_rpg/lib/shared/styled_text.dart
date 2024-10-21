ae386bb78ba4e345a485097ec3561be5862f1d52
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2024-10-21 14:01:39 +0700

    error

--- flutter_rpg/lib/shared/styled_text.dart
+++ flutter_rpg/lib/shared/styled_text.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText(this.text,{super.key});
+  const StyledText(this.text, {super.key});
+
   final String text;
+
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: Theme.of(context).textTheme.bodyMedium);
+    return Text(text, style: GoogleFonts.kanit(
+      textStyle: Theme.of(context).textTheme.bodyMedium,
+    ));
   }
 }
-
 
 class StyledHeading extends StatelessWidget {
-  const StyledHeading(this.text,{super.key});
+  const StyledHeading(this.text, {super.key});
+
   final String text;
+
   @override
   Widget build(BuildContext context) {
-    return Text(text.toUpperCase(), style: Theme.of(context).textTheme.headlineMedium);
+    return Text(text.toUpperCase(), style: GoogleFonts.kanit(
+      textStyle: Theme.of(context).textTheme.headlineMedium
+    ));
+    // return Text(
+    //   'This is Google Fonts',
+    //   style: GoogleFonts.lato(
+    //     textStyle: const TextStyle(color: Colors.blue, letterSpacing: .5),
+    //   ),
+    // );
   }
 }
 
-
 class StyledTitle extends StatelessWidget {
-  const StyledTitle(this.text,{super.key});
+  const StyledTitle(this.text, {super.key});
+
   final String text;
+
   @override
   Widget build(BuildContext context) {
-    return Text(text.toUpperCase(), style: Theme.of(context).textTheme.titleMedium);
+    return Text(text.toUpperCase(), style: GoogleFonts.kanit(
+      textStyle: Theme.of(context).textTheme.titleMedium
+    ));
   }
 }
