6afa014e251f1bd0fab47cb0d9f2c025cbd199a2
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2024-10-21 11:20:57 +0700

    error

--- flutter_rpg/lib/shared/styled_text.dart
+++ flutter_rpg/lib/shared/styled_text.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StyledText extends StatelessWidget {
   const StyledText(this.text,{super.key});
   final String text;
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: Theme.of(context).textTheme.bodyMedium);
+    return Text(text, style: GoogleFonts.lato(
+      textStyle: Theme.of(context).textTheme.bodyMedium
+    ));
   }
 }
 
