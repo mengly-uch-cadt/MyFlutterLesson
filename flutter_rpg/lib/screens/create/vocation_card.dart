4a6bfbb39bdc0a9b559691863f5ebf9aaf0bab45
Uch-Mengly <mengly_uch@student.tuxglobalinstitute.edu.kh>
2024-10-24 04:06:27 +0700

    gg

--- flutter_rpg/lib/screens/create/vocation_card.dart
+++ flutter_rpg/lib/screens/create/vocation_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class VocationCard extends StatelessWidget {
   const VocationCard({super.key, required this.vocation});
@@ -8,32 +9,38 @@ class VocationCard extends StatelessWidget {
   final Vocation vocation;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Row(
-          children: [
-            Image.asset('assets/img/vocations/${vocation.image}',
-              width: 80,
-              colorBlendMode: BlendMode.color,
-            ),
-            const SizedBox(width: 10,),
-
-            Expanded(
-              child:Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  StyledHeading(vocation.title),
-                  StyledText(vocation.description)
-                ],
-            ) 
-            
-            )
-            
-          ],
+    return GestureDetector(
+      onTap: (){
+        print(vocation.title);
+      },
+      child: Card(
+        color: AppColors.secondaryColor,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Image.asset('assets/img/vocations/${vocation.image}',
+                width: 80,
+                colorBlendMode: BlendMode.color,
+              ),
+              const SizedBox(width: 10,),
+      
+              Expanded(
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    StyledHeading(vocation.title),
+                    StyledText(vocation.description)
+                  ],
+              ) 
+              
+              )
+              
+            ],
+          ),
         ),
+      
       ),
-
     );
   }
 }
